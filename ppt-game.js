dade221a23ec9cd2ef023b286658654e22178d18
Felipe Vargas <felipevargas.bz@gmail.com>
2022-07-26 14:37:55 -0500

    ppt game

--- ppt-game.js
+++ ppt-game.js
@@ -6,26 +6,17 @@ function ramdom_select_ppt() {
 
 function validate(user, machine) {
 
-  if (user == machine){
-    return ('you chose: ' + user + '\n' + 'The machine chose: ' + machine + '\n' + 'Tie!!');
-
-  } else if (user == 'Piedra' && machine == 'Tijera') {
-     return ('you chose: ' + user + '\n' + 'The machine chose: ' + machine + '\n' + 'User Win!!');
-
-  } else if (user == 'Tijera' && machine === 'Papel') {
-     return ('you chose: ' + user + '\n' + 'The machine chose: ' + machine + '\n' + 'User Win!!');
-
-  } else if (user == 'Papel' && machine == 'Piedra') {
-     return ('you chose: ' + user + '\n' + 'The machine chose: ' + machine + '\n' + 'User Win!!');
-
-  } else if (user == 'Tijera' && machine == 'Piedra') {
-     return ('you chose: ' + user + '\n' + 'The machine chose: ' + machine + '\n' + 'Machine Win!!');
-
-  } else if (user == 'Papel' && machine == 'Tijera') {
-    return ('you chose: ' + user + '\n' + 'The machine chose: ' + machine + '\n' + 'Machine Win!!');
-
-  } else if (user == 'Piedra' && machine == 'Papel') {
-    return ('you chose: ' + user + '\n' + 'The machine chose: ' + machine + '\n' + 'Machine Win!!');
+  switch (true) {
+      case user === machine:
+        return 'Tie!!'
+      case user === 'Piedra' && machine === 'Tijera':
+        return 'User Win!!'
+      case user === 'Tijera' && machine === 'Papel':
+        return 'User Win!!'
+      case user === 'Papel' && machine === 'Piedra':
+        return 'User Win!!'
+      default:
+        return 'Machine Win!!'
   }
 }
 
@@ -39,6 +30,14 @@ function game(n) {
   if (num >= 0 && num <= options.length) {
       var user_option = options[num];
       console.log(machine_option + ' vs ' + user_option)
-      alert(validate(user_option, machine_option));
+      alert(
+        'you chose: '
+        + user_option
+        + '\n'
+        + 'The machine chose: '
+        + machine_option
+        +'\n'
+        + validate(user_option, machine_option)
+      );
   }
 }
